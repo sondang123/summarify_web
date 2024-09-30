9fcec76bea9cdae2f4963492b5fdaefab54d5bfa
sondang <sondang2kk@gmail.com>
2024-09-30 13:06:45 +0700

    fix: LottiePlayer

--- app/components/app-components/app-loading.tsx
+++ app/components/app-components/app-loading.tsx
@@ -1,12 +1,15 @@
 import { loadingAnimation } from '@/const/animation'
-import Lottie from 'react-lottie-player'
+import React, { Suspense } from 'react'
+const LottiePlayer = React.lazy(() => import('react-lottie-player'))
 export const AppLoading: React.FC = () => {
   return (
-    <Lottie
-      loop
-      animationData={loadingAnimation}
-      play
-      style={{ width: 150, height: 100 }}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <LottiePlayer
+        loop
+        animationData={loadingAnimation}
+        play
+        style={{ width: 150, height: 100 }}
+      />
+    </Suspense>
   )
 }
