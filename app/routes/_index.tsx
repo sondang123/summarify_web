4d063a945eca508b37b77d6e269404ef21bcd333
sondang <sondang2kk@gmail.com>
2024-10-01 17:00:05 +0700

    feat: change

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,6 +6,7 @@ import { VideoSummaries } from '@/components/feature/video-summaries'
 import { setToken } from '@/helper/helper-token'
 
 import { type LoaderFunction, json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 
 import { useEffect } from 'react'
 
@@ -18,19 +19,22 @@ type LoaderData = {
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url)
   const token = url.searchParams.get('token')
-  if (token) {
-    setToken(token)
-  }
+
   return json<LoaderData>({ token })
 }
 
 const HomePage = () => {
+  const { token } = useLoaderData<LoaderData>()
+
   useEffect(() => {
+    if (token) {
+      setToken(token)
+    }
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     })
-  }, [])
+  }, [token])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-main-background  bg-contain bg-top bg-no-repeat pt-28 bg-[url('./assets/images/bg-home.svg')]">
       <div className="container">
